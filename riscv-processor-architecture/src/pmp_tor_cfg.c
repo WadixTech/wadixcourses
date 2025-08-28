06c0a9ebd62d3279cf991d1188af2dd757255df4
Wassim Dhokar <rashen.tivani@gmail.com>
2025-08-28 22:27:38 +0200

    Add copyright header to all sources/headers Labs files

--- riscv-processor-architecture/src/pmp_tor_cfg.c
+++ riscv-processor-architecture/src/pmp_tor_cfg.c
@@ -1,6 +1,23 @@
+/*
+ * Copyright (c) 2025 Wadix Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <stdio.h>
 #include <stdint.h>
-
 
 __attribute__((always_inline)) void __set_pmp_tor_reg_0(uint32_t pmp_reg_base_addr, uint32_t pmp_reg_top_addr, uint32_t configuration)
 {
