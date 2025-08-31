6ace626121d27116f2b04656e2f398a49852c7d0
Wassim Dhokar <rashen.tivani@gmail.com>
2025-08-31 22:17:08 +0200

    update batch scripts

--- TinyML-tflm-ethos-npu/ethos-u-alif_kws_appkit.bat
+++ TinyML-tflm-ethos-npu/ethos-u-alif_kws_appkit.bat
@@ -1,3 +1,4 @@
-APFW0001-app-release-exec-windows-SE_FW_1.107.00_DEV\app-release-exec\app-gen-toc -f prebuild_images\config\kws_demo.json
-APFW0001-app-release-exec-windows-SE_FW_1.107.00_DEV\app-release-exec\app-write-mram -p
-
+cp -r prebuild_images/images/*  APFW0001-app-release-exec-windows-SE_FW_1.107.00_DEV/app-release-exec\build\images/
+cd  APFW0001-app-release-exec-windows-SE_FW_1.107.00_DEV\app-release-exec
+app-gen-toc -f ..\..\prebuild_images\config\kws_demo.json
+app-write-mram -p
